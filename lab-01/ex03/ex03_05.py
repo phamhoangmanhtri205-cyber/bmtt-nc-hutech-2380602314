90894a9ef44fab2acf8a689eed41acb2d7cbc43a
Tri <phamhoangmanhtri205@gmail.com>
2026-05-19 23:39:28 +0700

    Done

--- lab-01/ex03/ex03_05.py
+++ lab-01/ex03/ex03_05.py
@@ -4,7 +4,7 @@ def dem_so_lan_xuat_hien(lst):
         if item in count_dict:
             count_dict[item] += 1
         else:
-            count_dict[item] += 1
+            count_dict[item] = 1
     return count_dict
 input_string = input("Nhập danh sách các từ, cách nhau bằng dấu cách: ")
 world_list = input_string.split()
